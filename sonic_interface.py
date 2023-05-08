cada0d75f47f841d36522cc80de795736b01b168
mkskitka <mkskitka@gmail.com>
2023-05-07 20:26:28 -0400

    new sonicpi_interface code

--- sonic_interface.py
+++ sonic_interface.py
@@ -7,7 +7,7 @@ import serial
 
 print("version: ")
 print(serial.__version__)
-print(" ...end")
+print("waiting for input ...")
 
 ser = serial.Serial(
 	port='/dev/ttyACM0',
@@ -17,45 +17,71 @@ ser = serial.Serial(
 	bytesize=serial.EIGHTBITS,
 	timeout=1
 )
+
 ser.flush()
 ser.flushInput()
     
-curr_bin = []
-bin_averages = []
-# num_bins = 10
-numReadingsInBin = 100
-
-#PARAMETERS TO CONFIGUREcOn
-MIN_CIRCLE_BINS = 10; # min number of steps needed to complete a circle 
-#DIFF_CAP = 360 / MIN_CIRCLE_BINS; # max difference between circle steps 
-DIFF_CAP = 120
-DIFF_MIN = 5 # min number of degrees needed to turn to count as a step 
-
-CONSEC_CIRCLE_BINS = 0; 
-# roll states (increasing/ decreasing corresponds to roughly left turn/ right turning )
-INCREASING = "increasing"
-DECREASING = "decreasing"  
-ROLL_STATE = INCREASING; # going to be Positive, Negative, or None 
-
-# Algorithm for detecting a loop 
-    # Increasing consequetively for at least MIN_CIRCLE_BINS averages
-    # crosses past start point 
-
 
 sleep_intervals = ["0.25", "0.5", "0.75"]
 i = 0
+started = False
+
+
+
+#COMMANDS
+START = "start"
+STOP = "stop" 
+#JUMPS 
+UP_JUMP = "up_jump"
+FORWARD_JUMP = "forard_jump"
+SIDE_JUMP = "side_jump"
+#BENDS
+RIGHT_BEND = "right_bend"
+LEFT_BEND = "left_bend"
+#OTHER
+CIRCLE ="circle"
+
+
+# if statement states
+NONE = "none"
+STARTED = "started"
+IMPLEMENTED = "implemented"
+#if statement conditional options 
+# FORWARD_JUMP || SIDE_JUMP 
+if_statement = NONE
+if_statement_conditional = NONE
+
+IF_BUFFER = "9"
+MAIN_BUFFER = "0"
+
+
+
+#SONIC PI EXECUATABLES 
+
+def run_script(): 
+    hotkey("alt", "r")
+
+def stop_script(): 
+    hotkey("alt", "s")
+    
+def clear_script():
+    hotkey("alt", "a")
+    time.sleep(.3)
+    press('delete')
+    time.sleep(1)
 
 def enter():
     time.sleep(.3)
     press('return')
     time.sleep(.3)
 
-def play_note(): 
+def switch_buffer(n):
+    hotkey("alt", "shift", n)
 
+def play_note(): 
     midi_note = random.randint(60, 80)
-    write('play ' + str(midi_note))
+    writ('play ' + str(midi_note))
     enter()
-    51.78
 
 def sleepRand(): 
     sleep_i_idx = random.randint(0, 2)
@@ -65,6 +91,8 @@ def sleepRand():
     
     
 def write_loop(): 
+    switch_buffer(MAIN_BUFFER)
+    global i
     write("live loop :" + str(i) + " do")
     enter()
     midi_note = random.randint(60, 80)
@@ -78,7 +106,30 @@ def write_loop():
 
     i += 1
 
+def write_if(command):
+    print("write_if") 
+    switch_buffer(IF_BUFFER)
+    time.sleep(.3)
+    hotkey('ctrl', 'k')
+    time.sleep(.3)
+    write(command + " =true")
+    enter()
+    write("play " + str(random.randint(50, 70)) +" , amp: 2 if " + command) 
+    time.sleep(.3)
+    hotkey("alt", "shift", "u")
+    run_script()
+    
+def write_if_conditional(command):
+    switch_buffer(IF_BUFFER)
+    run_script()
+
+    
+    
+            
+    
+
 def do_times(n): 
+    switch_buffer(MAIN_BUFFER)
     write("use_random_seed " + str(n))
     enter()
     write(str(n) + ".times do")
@@ -91,74 +142,7 @@ def do_times(n):
     write("end")
     enter()
     run_script()
-
-def run_script(): 
-    hotkey("alt", "r")
-
-def stop_script(): 
-    hotkey("alt", "s")
     
-            
-def detectedCircle():
-    print("\n\n***********************************************************")
-    print("\n\n DETECTED CIRCLE !!! \n\n")
-    print("************************************************************\n\n")
-    play_note()
-    sleepRand()
-    run_script()
-    # reset circle tracking 
-    CONSEC_CIRCLE_BINS = 0
-    circle_start = bin_averages[len(bin_averages)-1]
-    
-def isNeg(n):
-    return n < 0
-def isPos(n): 
-    return n >= 0
-def isAtEdge(n1, n2):
-    if(n1 >= 180-DIFF_CAP and n2 <= -180 + DIFF_CAP) or (n1 <= -180 + DIFF_CAP and n2 >= 180-DIFF_CAP):
-            return True;
-    else: 
-        return False; 
-        
-    
-def checkForCircle():
-    if(len(curr_bin) > 1):
-            global CONSEC_CIRCLE_BINS
-            global ROLL_STATE
-            prev_bin = curr_bin[len(curr_bin)-2]
-            now_bin = curr_bin[len(curr_bin)-1]
-            if(isAtEdge(prev_bin, now_bin) and isNeg(prev_bin)):
-                print("is at edge - dec")
-                
-                rel_to_last = DECREASING
-                diff_from_last = (180 + prev_bin) +  (180 - now_bin) 
-                
-                print(ROLL_STATE == rel_to_last)
-                print("diff to last" + str(diff_from_last))
-            elif(isAtEdge(prev_bin, now_bin) and isPos(prev_bin)):
-                print("is at edge - inc")
-                rel_to_last = INCREASING
-                diff_from_last = (180 + now_bin) +  (180 - prev_bin) 
-                print("diff to last" + str(diff_from_last))
-                print(ROLL_STATE == rel_to_last)
-            else:
-                diff_from_last = prev_bin - now_bin
-                if(diff_from_last < 0):
-                    rel_to_last = INCREASING
-                else:
-                    rel_to_last = DECREASING
-            
-            if(abs(diff_from_last) <= DIFF_CAP and  abs(diff_from_last) >= DIFF_MIN):
-                print("consec circle bins: " + str(CONSEC_CIRCLE_BINS))
-                CONSEC_CIRCLE_BINS += 1
-            else:
-                if(CONSEC_CIRCLE_BINS >= MIN_CIRCLE_BINS):
-                    do_times(CONSEC_CIRCLE_BINS)
-                # reset circle tracking 
-                ROLL_STATE = rel_to_last  
-                CONSEC_CIRCLE_BINS = 0
-                circle_start = curr_bin[len(bin_averages)-1]
-
     
 rnum = 0
 ii = 0
@@ -166,27 +150,62 @@ ii = 0
 while 1: 
     try: 
         x=ser.readline().strip()
-        #val = x.decode('utf-8')
-        #print(x)
-        val = float(x)
+        val = x.decode('utf-8')
+        if(val == ''):
+            continue
+        #print(val)
+        val_parsed = val.split(':')
+        command = val_parsed[0]
+        if(len(val_parsed) > 1):
+            magnitude = float(val_parsed[1])
+            
     except ValueError: 
        print("value error!")
        continue
     
-    #print(val)
-    #print(str(rnum) + ", " +  str(val))
-    rnum += 1
-    rel_to_last = None; 
-    diff_from_last = None; 
-    if(ii % numReadingsInBin == 0):
-        #average = round(sum(curr_bin)/len(curr_bin), 2)
-        #print(average)
-        #print(val)
-        #bin_averages.append(average)
-        curr_bin.append(val)
-        checkForCircle()
-    #else: 
-        #curr_bin.append(round(float(val), 2))
+    if(command == START):
+        started = True
+    else:
+        if(command == CIRCLE):
+            print("detected ", CIRCLE, " ->  FOR LOOP")
+            do_times(magnitude)
+        if(command == UP_JUMP):
+            print("detected ", UP_JUMP, " -> CLEAR CODE")
+            if_statement = NONE
+            clear_script()
+            
+        if(command == SIDE_JUMP):
+            print("detected ", SIDE_JUMP, " -> if:" + if_statement, " , ic: ", if_statement_conditional)
+            if if_statement == STARTED:
+                if_statement = IMPLEMENTED
+                if_statement_conditional = SIDE_JUMP
+                write_if(SIDE_JUMP)
+            elif if_statement == IMPLEMENTED and if_statement_conditional == SIDE_JUMP:
+                write_if_conditional(SIDE_JUMP)
+            else: 
+                print("")
+        if(command == FORWARD_JUMP):
+            print("detected ", FORWARD_JUMP, " -> " + if_statement, " , ic: ", if_statement_conditional)
+            if if_statement == STARTED:
+                if_statement = IMPLEMENTED
+                if_statement_conditional = FORWARD_JUMP
+                write_if(FORWARD_JUMP)
+            elif if_statement == IMPLEMENTED and if_statement_conditional == FORWARD_JUMP:
+                write_if_conditional(FORWARD_JUMP)
+            else: 
+                print("")
+            
+        if(command == RIGHT_BEND):
+            print("detected ", RIGHT_BEND, " -> IF STATEMENT")
+            if_statement = STARTED
+            
+        if(command == LEFT_BEND):
+            print("detected ", LEFT_BEND, " -> STOP")
+            clear_script()
+            run_script()
+            started = False
+                
+        
         
     ii = ii + 1
         
